10ffda18576d5d8a5224db0ce980c61f3408e60a
jdonic <jdoni@jdonic.fedora>
2023-01-16 00:11:15 +0100

    Create detail URLs
    Create urls and views for displaying details about
    each model and a specific model as well

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
-from .views import AttributeValueCreateView
+from .views import (
+    ImportCreateView,
+    AttributeValueListView,
+    AttributeValueDetailView,
+    CatalogListView,
+    CatalogDetailView,
+)
 
 urlpatterns = [
-    path("import/", AttributeValueCreateView.as_view(), name="import"),
+    path("import/", ImportCreateView.as_view(), name="import"),
+    path("detail/attribute_value/", AttributeValueListView.as_view(), name="attr_list"),
+    path(
+        "detail/attribute_value/<int:pk>/",
+        AttributeValueDetailView.as_view(),
+        name="attr_detail",
+    ),
+    path("detail/catalog/", CatalogListView.as_view(), name="catalog_list"),
+    path(
+        "detail/catalog/<int:pk>/", CatalogDetailView.as_view(), name="catalog_detail"
+    ),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,21 @@
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
+from django.http import HttpRequest
 
 from .serializers import AttributeValueSerializer, CatalogSerializer
+from .models import AttributeValue, Catalog
+
+from typing import Optional
+
 import json
 
 
-class AttributeValueCreateView(APIView):
+class ImportCreateView(APIView):
     parser_classes = [JSONParser]
 
-    def post(self, request, format=None) -> None:
+    def post(self, request: HttpRequest, format: Optional[str] = None) -> None:
         for item in request.data:
             key = list(item.keys())[0]
 
@@ -30,3 +36,23 @@ class AttributeValueCreateView(APIView):
                     serializer.save()
 
         return Response({"received data": request.data})
+
+
+class AttributeValueListView(generics.ListAPIView):
+    queryset = AttributeValue.objects.all()
+    serializer_class = AttributeValueSerializer
+
+
+class AttributeValueDetailView(generics.RetrieveAPIView):
+    queryset = AttributeValue.objects.all()
+    serializer_class = AttributeValueSerializer
+
+
+class CatalogListView(generics.ListAPIView):
+    queryset = Catalog.objects.all()
+    serializer_class = CatalogSerializer
+
+
+class CatalogDetailView(generics.RetrieveAPIView):
+    queryset = Catalog.objects.all()
+    serializer_class = CatalogSerializer
