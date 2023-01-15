4f695698b86291805c7971b7a633756c43382828
jdonic <jdoni@jdonic.fedora>
2023-01-15 18:02:58 +0100

    Automate tests and linters
    Automate tests and linters using tox.ini file and setup
    Add an non existing App as a placeholder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Models_api app",
+    version="1.0.0",
+    author="Jakub Donič",
+    author_email="donicjakub@gmail.com",
+    description=("A django app for inserting and parsing JSON, saving it to models and displaying it."),
+    url="https://github.com/jdonic/Movies",
+)
