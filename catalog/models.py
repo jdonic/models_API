6ac0c2016fdc64232388674a9d83869b6d2316a2
jdonic <jdoni@jdonic.fedora>
2023-01-15 23:23:38 +0100

    Add models and migrations files

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class AttributeValue(models.Model):
+    id = models.IntegerField(primary_key=True)
+    hodnota = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.hodnota
+
+
+class Catalog(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nazev = models.CharField(max_length=255)
+    products_ids = models.JSONField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.nazev
